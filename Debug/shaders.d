ad63bdb6d659c60cf39ee9c24977ca7e181b2085
ryankeedy <ryankeedy@users.noreply.github.com>
2015-10-01 22:54:49 -0700

    First commit

---
+++ Debug/shaders.d
@@ -0,0 +1,1 @@
+/shaders.o : ../shaders.cpp
---
+++ Release/subdir.mk
@@ -0,0 +1,49 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../particleSystem.cpp \
+../particles.cpp \
+../render_particles.cpp \
+../shaders.cpp 
+
+CU_SRCS += \
+../particleSystem_cuda.cu 
+
+CU_DEPS += \
+./particleSystem_cuda.d 
+
+OBJS += \
+./particleSystem.o \
+./particleSystem_cuda.o \
+./particles.o \
+./render_particles.o \
+./shaders.o 
+
+CPP_DEPS += \
+./particleSystem.d \
+./particles.d \
+./render_particles.d \
+./shaders.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	nvcc -I"/home/rkeedy/NVIDIA_CUDA-5.0_Samples/5_Simulations" -I"/home/rkeedy/NVIDIA_CUDA-5.0_Samples/common/inc" -I"/home/rkeedy/workspace/BIFF" -O3 -gencode arch=compute_20,code=sm_20 -gencode arch=compute_20,code=sm_21 -odir "" -M -o "$(@:%.o=%.d)" "$<"
+	nvcc -I"/home/rkeedy/NVIDIA_CUDA-5.0_Samples/5_Simulations" -I"/home/rkeedy/NVIDIA_CUDA-5.0_Samples/common/inc" -I"/home/rkeedy/workspace/BIFF" -O3 --compile  -x c++ -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+%.o: ../%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	nvcc -I"/home/rkeedy/NVIDIA_CUDA-5.0_Samples/5_Simulations" -I"/home/rkeedy/NVIDIA_CUDA-5.0_Samples/common/inc" -I"/home/rkeedy/workspace/BIFF" -O3 -gencode arch=compute_20,code=sm_20 -gencode arch=compute_20,code=sm_21 -odir "" -M -o "$(@:%.o=%.d)" "$<"
+	nvcc --compile -I"/home/rkeedy/NVIDIA_CUDA-5.0_Samples/5_Simulations" -I"/home/rkeedy/NVIDIA_CUDA-5.0_Samples/common/inc" -I"/home/rkeedy/workspace/BIFF" -O3 -gencode arch=compute_20,code=compute_20 -gencode arch=compute_20,code=sm_21  -x cu -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
